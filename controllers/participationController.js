c165c9b5e16c73bf5ee5364fe361d7ec9f376a7c
KTokarski2 <krzysztof.tokarski2@icloud.com>
2022-12-22 12:04:44 +0100

    connected participation API to it's template, fixed some bugs

--- controllers/participationController.js
+++ controllers/participationController.js
@@ -1,11 +1,100 @@
+const ParticipationRepository = require('../repository/sequelize/ParticipationRepository');
+const PoliceOfficerRepository = require('../repository/sequelize/PoliceOfficerRepository');
+const CaseRepository = require('../repository/sequelize/CaseRepository');
+
 exports.showParticipationList = (req, res, next) => {
-    res.render('pages/Participation/list', {navLocation: 'participation'});
-}
+    ParticipationRepository.getParticipations()
+        .then(participations => {
+            res.render('pages/Participation/list', {
+                participations: participations,
+                navLocation: 'participation'
+            });
+        });
+};
 
 exports.showAddParticipationForm = (req, res, next) => {
-    res.render('pages/Participation/form', {navLocation: 'participation'});
-}
+    let allPoliceOfficers, allCases;
+    PoliceOfficerRepository.getPoliceOfficers()
+        .then(policeOfficers => {
+            allPoliceOfficers = policeOfficers;
+            return CaseRepository.getCases();
+        })
+        .then(cases => {
+            allCases = cases;
+            res.render('pages/Participation/form', {
+                participation: {},
+                formMode: 'createNew',
+                allPoliceOfficers: allPoliceOfficers,
+                allCases: allCases,
+                pageTitle: 'Nowy udział w sprawie',
+                btnLabel: 'Przydziel policjanta do sprawy',
+                formAction: '/Participation/add',
+                navLocation: 'participation'
+            });
+        });
+};
 
 exports.showParticipationDetails = (req, res, next) => {
-    res.render('pages/Participation/details', {navLocatoin: 'participation'});
-}
+    const participationId = req.params.participationId;
+    ParticipationRepository.getParticipationById(participationId)
+        .then(participation => {
+            res.render('pages/Participation/form', {
+                participation: participation,
+                formMode: 'showDetails',
+                pageTitle: 'Szczegóły udziału w sprawie',
+                formAction: '',
+                navLocation: 'participation'
+            });
+        });
+};
+
+exports.showEditParticipationForm = (req, res, next) => {
+    const participationId = req.params.participationId;
+    let allPoliceOfficers, allCases;
+    PoliceOfficerRepository.getPoliceOfficers()
+        .then(policeOfficers => {
+            allPoliceOfficers = policeOfficers;
+            return CaseRepository.getCases();
+        })
+        .then(cases => {
+            allCases = cases;
+            return ParticipationRepository.getParticipationById(participationId);
+        })
+        .then(participation => {
+            res.render('pages/Participation/form', {
+                participation: participation,
+                formMode: 'edit',
+                allPoliceOfficers: allPoliceOfficers,
+                allCases: allCases,
+                pageTitle: 'Edycja udziału w sprawie',
+                btnLabel: 'Edytuj udział w sprawie',
+                formAction: '/Participation/edit',
+                navLocation: 'participation'
+            });
+        });
+};
+
+exports.addParticipation = (req, res, next) => {
+    const participationData = { ...req.body };
+    ParticipationRepository.createParticipation(participationData)
+        .then(result => {
+            res.redirect('Participation')
+        });
+};
+
+exports.updateParticipation = (req, res, next) => {
+    const participationId = req.body._id;
+    const participationData = { ...req.body };
+    ParticipationRepository.updateParticipation(participationId, participationData)
+        .then( result => {
+            res.redirect('/Participation');
+        });
+};
+
+exports.deleteParticipation = (req, res, next) => {
+    const participationId = req.params.participationId;
+    ParticipationRepository.deleteParticipation(participationId)
+        .then( () => {
+            res.redirect('/Participation');
+        });
+};
--- routes/participationRoute.js
+++ routes/participationRoute.js
@@ -4,6 +4,10 @@ const participationController = require('../controllers/participationController'
 
 router.get('/', participationController.showParticipationList);
 router.get('/add', participationController.showAddParticipationForm);
+router.post('/add', participationController.addParticipation);
+router.post('/edit', participationController.updateParticipation);
+router.get('/edit/:participationId', participationController.showEditParticipationForm);
 router.get('/details/:participationId', participationController.showParticipationDetails);
+router.get('/delete/:participationId', participationController.deleteParticipation);
 
 module.exports = router;
